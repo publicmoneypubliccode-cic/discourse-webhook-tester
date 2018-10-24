a8681710c001294d3a933818478269bd28f8a635
Marcus Baw <marcusbaw@gmail.com>
2018-10-24 10:50:50 +0100

    added logic to check it is monday, and logging

--- batsignal-tester.rb
+++ batsignal-tester.rb
@@ -3,26 +3,40 @@
 require 'discourse_api'
 require 'date'
 require 'dotenv'
+require 'logger'
 Dotenv.load('test-env')
 
-# set up client
-client = DiscourseApi::Client.new(ENV['DISCOURSE_URL'])
-client.api_key, client.api_username = ENV['DISCOURSE_API_KEY'], ENV['DISCOURSE_API_USER']
+@logger = Logger.new(STDOUT)
 
-# create new topic on Discourse
-begin
-  client.create_topic(
-    category: ENV['DISCOURSE_CATEGORY'],
-    skip_validations: true,
-    auto_track: false,
-    title: ENV['BATSIGNAL_TEST_TITLE'] + " " + DateTime.now.strftime("%Y.%m.%d"),
-    raw: ENV['BATSIGNAL_TEST_TEXT']
-  )
-rescue DiscourseApi::UnprocessableEntity => error
-  # `body` is something like `{ errors: ["Name must be at least 3 characters"] }`
-  # This outputs "Name must be at least 3 characters"
-  # TODO email errors to admin
-  puts error.response.body['errors'].first
+def send_batsignal
+  # set up client
+  client = DiscourseApi::Client.new(ENV['DISCOURSE_URL'])
+  client.api_key, client.api_username = ENV['DISCOURSE_API_KEY'], ENV['DISCOURSE_API_USER']
+
+  # create new topic on Discourse
+  begin
+    client.create_topic(
+      category: ENV['DISCOURSE_CATEGORY'],
+      skip_validations: true,
+      auto_track: false,
+      title: ENV['BATSIGNAL_TEST_TITLE'] + " " + DateTime.now.strftime("%Y.%m.%d"),
+      raw: ENV['BATSIGNAL_TEST_TEXT']
+    )
+    @logger.info "BATSIGNAL TEST SENT - #{Date.today.strftime('%A')} #{DateTime.now.strftime("%Y.%m.%d")} CATEGORY: #{ENV['DISCOURSE_CATEGORY']}"
+  rescue DiscourseApi::UnprocessableEntity => error
+    # `body` is something like `{ errors: ["Name must be at least 3 characters"] }`
+    # This outputs "Name must be at least 3 characters"
+    # email errors to admin
+    puts error.response.body['errors'].first
+    @logger.error "Batsignal sending errored: #{error.response.body['errors'].first}"
+  end
 end
 
-# TODO email notification to admin
+# check it is Monday
+if Date.today.monday?
+  send_batsignal
+else
+  @logger.info "Batsignal test not sent - today is #{Date.today.strftime('%A')}"
+end
+
+# email notification to admin
